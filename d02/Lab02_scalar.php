855bb0e687f4d362a8b85bb73050aba14a9d04b1
IRIS <bebebietyeu0903@gmail.com>
2020-04-03 09:07:17 +0700

    Add files via upload

---
+++ d02/Lab02_scalar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Scalar Type</title>
+    </head>
+    <body
+        <h2>Scalar Type Demo</h2>
+        <h3>Without Declaration</h3>
+        <hr>
+        <?php
+            function cong($x,$y){
+                echo "$x + $y = " . ($x + $y) . "<br>";
+            }
+            
+            $n1=5;
+            $n2=10;
+            cong($n1,$n2);
+        ?>
+    </body>
+</html>
