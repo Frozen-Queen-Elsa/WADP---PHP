bd15779ece60be5b472d9e8e925389cfff450c91
IRIS <bebebietyeu0903@gmail.com>
2020-04-18 21:53:21 +0700

    Add files via upload

---
+++ Ass01/Ass01_NewBook.php
@@ -0,0 +1,43 @@
+<?php
+    include_once '../ThuVien/connectDB_Ass01.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        
+        if (isset($_GET["btnOK"]) == FALSE) {
+            header("location:Ass01_NewBook.html");
+            exit();
+        }
+        
+        $name = $_GET["txtName"];
+        $author = $_GET["txtAuthor"];
+        $price = $_GET[txtPrice];
+        
+        $sql = "insert into tbbook(name,price,author) values('$name',$price,'$author');";
+       
+        //thuc hien lenh truy van SQL
+        $r = mysqli_query($link, $sql);
+        
+        //kiem tra xem co dong du lieu dc tra ve tu cau linh Select SQL ?
+        if ($r==TRUE) {
+            //Open trang danh sách lớp học
+            header("location:Ass01_ViewBook.php");
+            exit();
+        }
+        echo "<h3>Lỗi sai DB : Không thể thêm Book mới !</h3>"
+        ?>
+        
+        
+        
+        
+    </body>
+</html>
---
+++ Ass01/Ass01_ViewBook.php
@@ -0,0 +1,70 @@
+<?php
+    include_once '../ThuVien/connectDB_Ass01.php';
+    
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Book List</title>
+<!--        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>-->
+        <!-- Font Google -->
+        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+        <link href="Ass01_Table.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <h2>Book List</h2>
+            <span>
+                <a href="Ass01_NewBook.php">Create New</a> <br><br>
+            </span>
+            
+            <form action="Ass01_SearchBook.php">
+                <h3 style="margin-left: 12%;margin-bottom: 2%">
+                    Price
+                    <span style="color:green">From $ </span>
+                    <input style="margin-left:3%;width: 130px;height: 25px;text-align: center;font-weight: 500;font-size:17px" class="ipNum" type="number" min="0" name="txtMin" id="txtMin"> 
+                    <span style="color:green">To $ </span> 
+                    <input style="margin-left:3%;width: 130px;height: 25px;text-align: center;font-weight: 500;font-size:17px" class="ipNum" type="number"  min="0" name="txtMax" id="txtMax"> 
+                    <input style="margin-left:7%;width: 80px;height: 25px;text-align: center;background-color: pink;color:rgb(126, 126, 235);font-weight: 700;font-size:17px" type="submit" name="btnSearch" value="Search">
+                </h3>
+            </form>
+            
+            <hr>
+            
+            <table class="table tab-content ">
+                <thead>
+                    <tr>
+                        <th>BookID</th>
+                        <th>Book Name</th>
+                        <th>Price</th>
+                        <th>Author</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $sql="select * from tbbook where price";
+                        $r= mysqli_query($link, $sql);
+
+                        $a_book= mysqli_fetch_all($r);
+                        foreach ($a_book as $item) {
+                          echo '<tr>';
+                          echo "<td> $item[0] </td>";
+                          echo "<td> $item[1] </td>";
+                          echo "<td> $item[2] </td>";
+                          echo "<td> $item[3] </td>";
+                          echo "<td>";
+                          echo '</tr>';
+    
+                        }
+                    ?>
+                </tbody>
+            </table>
+            
+            
+            
+        </div>
+    </body>
+</html>
